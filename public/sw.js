a0f1e429f39dc0d9739608b2c17a5f88a8cf5e64
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2021-08-30 09:21:30 +0530

    added start script to start app on heroku serever

--- public/sw.js
+++ public/sw.js
@@ -19,66 +19,44 @@ const itemsCached = [
 
 
 
-// self.addEventListener('install', e => {
-//     e.waitUntil(
-//         caches.open("c1").then(cache => {
-//             return cache.addAll(itemsCached)
-//         })
-//         .then(self.skipWaiting())
-//     )
-
-// })
-
-
-// self.addEventListener('fetch', e => {
-//     e.respondWith(
-//         caches.match(e.request).then(response => {
-//             return response || fetch(e.request)
-//         })
-//     )
-// })
-
-
-
-
 // self.addEventListener('activate', function(event) {
 //     console.log('this event triggers when the service worker activates')
 // })
 
 
 
-// self.addEventListener('install', function(event) {
-//     event.waitUntil(
-//         caches.open('c2')
-//         .then(function(e) {
-//             return e.addAll(itemsCached)
-//         }).then(self.skipWaiting())
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('c2')
+        .then(function(e) {
+            return e.addAll(itemsCached)
+        }).then(self.skipWaiting())
 
-//     )
-// })
-
-
-// self.addEventListener('activate', function(event) {
-//     event.waitUntil(
-//         caches.keys()
-//         .then((keyList) => {
-//             return Promise.all(keyList.map((key) => {
-//                 if (key !== 'c2') {
-//                     console.log('[ServiceWorker] Removing old cache', key)
-//                     return caches.delete(key)
-//                 }
-//             }))
-//         })
-//         .then(() => self.clients.claim())
-//     )
-// })
+    )
+})
 
 
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== 'c2') {
+                    console.log('[ServiceWorker] Removing old cache', key)
+                    return caches.delete(key)
+                }
+            }))
+        })
+        .then(() => self.clients.claim())
+    )
+})
 
-// self.addEventListener('fetch', e => {
-//   e.respondWith(
-//     caches.match(e.request).then( response => {
-//       return response || fetch(e.request)
-//     })
-//   )
-// })
+
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then( response => {
+      return response || fetch(e.request)
+    })
+  )
+})
